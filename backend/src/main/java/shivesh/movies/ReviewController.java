5736fae1394cf09ba1a0114a04f7760b446a9bfe
shivxsh <sreeshiveshpsg@gmail.com>
2024-06-24 16:28:26 +0530

    Fetching movies via Axios

--- backend/src/main/java/shivesh/movies/ReviewController.java
+++ backend/src/main/java/shivesh/movies/ReviewController.java
@@ -1,9 +1,9 @@
 package shivesh.movies;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +13,15 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/reviews")
+@CrossOrigin(origins = "*")
 public class ReviewController {
 
     @Autowired
     private ReviewService reviewService;
 
     @PostMapping
-    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload){
-        return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
+    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
+        return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")),
+                HttpStatus.CREATED);
     }
 }
